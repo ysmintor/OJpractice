5ea240cbb9a84888feb19557c8349d0aff4fa613
York <ysmintor@gmail.com>
2018-02-18 09:11:17 +0800

    add ch2_8

---
+++ chapter2/ch2_8.cpp
@@ -0,0 +1,20 @@
+//
+// Created by York on 2018/2/17.
+//
+#include <stdio.h>
+int main(){
+    int n;
+    while (scanf("%d", &n) != EOF && n != 0) {
+        int Hash[101] = {0};
+        for (int i = 0; i < n; ++i) {
+            int x;
+            scanf("%d", &x);
+            Hash[x]++;
+        }
+
+        int x;
+        scanf("%d", &x);
+        printf("%d\n", Hash[x]);
+    }
+    return 0;
+}
